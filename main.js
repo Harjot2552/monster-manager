2b47ec22a1336b7e9ba81f959b0ff9441a1c6dfc
Harjot <hajotsingh2552@gmail.com>
2025-03-20 16:58:03 -0600

    Got all the important values from document and stored them in a variables

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ function createMonster(event) {
     event.preventDefault();
 
  
+   // getting all the input field data.
+   const name = document.getElementById('name').value;
+   const type = document.querySelector('input[name="type"]:checked');
+   const rarity = document.getElementById('rarity').value;
+   const favorite = document.querySelector('input[name="favorite"]').checked;
+
 }
